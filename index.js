f23df7e08954f54b8c4fe3abb4e073250bf11995
Adnane Taleb <adnane102464@gmail.com>
2024-09-14 23:15:57 +0100

    Add effects to the icons in the cards

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let bookIcon = document.querySelector('.fa-book');
-let starIcon = document.querySelector('.star');
-let trashIcon = document.querySelector('.fa-trash-can');
+let bookIcon = document.querySelectorAll('.fa-book');
+let starIcon = document.querySelectorAll('.fa-star');
+let trashIcon = document.querySelectorAll('.fa-trash-can');
+
+let bookIcons = Array.from(bookIcon);
+let starIcons = Array.from(starIcon);
+let trashIcons = Array.from(trashIcon);
 
 let asideElement = document.querySelector('aside');
 let addBookButton = document.querySelector('.add-book-btn');
@@ -11,12 +15,28 @@ addBookButton.addEventListener('click', () => {
   });
 
 //add a book to the reading list
-bookIcon.addEventListener('click' , (e) => {
-    if(e.target.classList.contains('fa-book')){
-        e.target.classList.remove('fa-book');
-        e.target.classList.add('fa-book-open-reader');
-    }else{
-        e.target.classList.remove('fa-book-open-reader');
-        e.target.classList.add('fa-book');
-    }
+bookIcons.forEach((book) => {
+    book.addEventListener('click' , (e) => {
+        if(e.target.classList.contains('fa-book')){
+            e.target.classList.remove('fa-book');
+            e.target.classList.add('fa-book-open-reader');
+        }else{
+            e.target.classList.remove('fa-book-open-reader');
+            e.target.classList.add('fa-book');
+        }
+    })
+})
+
+
+//add a book to the favorite list
+starIcons.forEach((star) => {
+    star.addEventListener('click' , (e) => {
+        if(e.target.classList.contains('fa-regular')){
+            e.target.classList.remove('fa-regular');
+            e.target.classList.add('fa-solid');
+        }else{
+            e.target.classList.remove('fa-solid');
+            e.target.classList.add('fa-regular');
+        }
+    })
 })
