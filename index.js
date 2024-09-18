f0af08b96afeaf429d209dcb21d3edaa025fc250
Adnane <adnane102464@gmail.com>
2024-09-18 21:55:25 +0100

    Fetch information for the new book to be added to the library

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Icons for different manipulations for books 
 let bookIcon = document.querySelectorAll('.fa-book');
 let starIcon = document.querySelectorAll('.fa-star');
 let trashIcon = document.querySelectorAll('.fa-trash-can');
@@ -6,15 +7,19 @@ let bookIcons = Array.from(bookIcon);
 let starIcons = Array.from(starIcon);
 let trashIcons = Array.from(trashIcon);
 
+//Different sections of the website 
 let asideElement = document.querySelector('aside');
 let addBookButton = document.querySelector('.add-book-btn');
+
+//Target the form 
+let form = document.querySelector('form');
 
 
 addBookButton.addEventListener('click', () => {
     asideElement.classList.toggle('hide');
   });
 
-//add a book to the reading list
+//Add a book to the reading list
 bookIcons.forEach((book) => {
     book.addEventListener('click' , (e) => {
         if(e.target.classList.contains('fa-book')){
@@ -27,8 +32,7 @@ bookIcons.forEach((book) => {
     })
 })
 
-
-//add a book to the favorite list
+//Add a book to the favorite list
 starIcons.forEach((star) => {
     star.addEventListener('click' , (e) => {
         if(e.target.classList.contains('fa-regular')){
@@ -40,3 +44,20 @@ starIcons.forEach((star) => {
         }
     })
 })
+
+//Add a new book to the library 
+form.addEventListener('submit' , (e) => {
+
+    e.preventDefault();
+
+    let bookObject = {
+        title : document.querySelector('#title').value,
+        author : document.querySelector('#author').value,
+        genre : document.querySelector('#genre').value,
+        year : document.querySelector('#year').value,
+        ageRange : document.querySelector('#age-range').value
+    }
+
+    
+})
+
